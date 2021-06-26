a5cff61be60aadb4d081cbb9fb19c726f07c592b
m05e5 <calebmoses1111@gmail.com>
2021-06-26 14:04:53 +0100

    added the html, css and js to the project

---
+++ form_validation.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById('submit');
+const state = document.getElementById('form-state');
+state.style.color = 'red';
+submitBtn.onclick = function (event) {
+  const email = document.getElementById('email').value;
+  const lowerCaseEmail = email.tolowercase();
+  if (email !== lowerCaseEmail) {
+    state.textContent = 'Incorrect Email Format';
+    event.preventDefault();
+  }
+};
